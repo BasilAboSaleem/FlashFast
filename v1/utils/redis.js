2c652879a9c4b0e82f950fec9c089055f23f40ba
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-10-28 13:11:50 +0200

    fix redis in v1

--- v1/utils/redis.js
+++ v1/utils/redis.js
@@ -1,9 +1,12 @@
 import Redis from "ioredis";
 
-export const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+const redis = new Redis({
+  host: process.env.REDIS_HOST || "equipped-stallion-13425.upstash.io",
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || "ATRxAAIncDJjNzRiY2I0NmUzMDk0N2RhYTA2ODA2MTk5MmJkNDZmOXAyMTM0MjU",
+  tls: process.env.REDIS_HOST ? {} : undefined, 
 });
+
 
 redis.on("connect", () => console.log("✅ Redis connected"));
 redis.on("error", (err) => console.error("❌ Redis error:", err));
