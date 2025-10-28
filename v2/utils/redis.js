f9e158b271dbf2b57af84757060a9ce434402c22
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-10-28 11:17:06 +0200

    fix(redis): support local and Upstash TLS connections for v2

--- v2/utils/redis.js
+++ v2/utils/redis.js
@@ -1,10 +1,10 @@
 const Redis = require("ioredis");
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
+  host: process.env.REDIS_HOST || "equipped-stallion-13425.upstash.io",
   port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
-  tls: {}, // Enable TLS for secure connections (e.g., Upstash)
+  password: process.env.REDIS_PASSWORD || "ATRxAAIncDJjNzRiY2I0NmUzMDk0N2RhYTA2ODA2MTk5MmJkNDZmOXAyMTM0MjU",
+  tls: process.env.REDIS_HOST ? {} : undefined, 
 });
 
 redis.on("connect", () => {
